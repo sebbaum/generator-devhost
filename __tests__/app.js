92996832435b1df8232b38cb562f59eadb9fb263
Sebastian Baum <sebastian.baum@ion2s.com>
2017-11-28 09:45:38 +0100

    - implemented unit tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -3,15 +3,42 @@ const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
-// describe('generator-devhost:app', () => {
-//   beforeAll(() => {
-//     return helpers.run(path.join(__dirname, '../generators/app'))
-//       .withPrompts({someAnswer: true});
-//   });
-//
-//   it('creates files', () => {
-//     assert.file([
-//       'dummyfile.txt'
-//     ]);
-//   });
-// });
+const existingFiles = [
+  'devhost/settings.yml',
+  'devhost/Vagrantfile',
+  'devhost/chef/environments/.gitkeep',
+  'devhost/chef/nodes/.gitkeep',
+  'devhost/chef/data_bags/.gitkeep'
+];
+
+describe('generator-devhost:app', () => {
+  beforeAll(() => {
+    return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({
+      name: 'testbox',
+      servers: ['nginx', 'mysql'],
+      enableSSL: true,
+      dbName: 'testDB',
+      awsAccessKey: 'testKey',
+      awsSecretAccessKey: 'testSecKey'
+    });
+  });
+
+  it('creates files', () => {
+    assert.file(existingFiles);
+  });
+
+  it('should contain the boxname', () => {
+    assert.fileContent('devhost/settings.yml', /box-name: testbox/);
+    assert.fileContent('devhost/settings.yml', /nginx:\n {2}enabled: true/);
+    assert.fileContent('devhost/settings.yml', /- shortname: testbox/);
+    assert.fileContent('devhost/settings.yml', /ssl_enabled: true/);
+    assert.fileContent('devhost/settings.yml', /domains:\n {8}- dev.host/);
+    assert.fileContent('devhost/settings.yml', /mysql:\n {2}enabled: true/);
+    assert.fileContent('devhost/settings.yml', /root_password: root!/);
+    assert.fileContent('devhost/settings.yml', /admin_password: admin!/);
+    assert.fileContent('devhost/settings.yml', /databases:\n {4}- testDB/);
+    assert.fileContent('devhost/settings.yml', /build-tools:\n {2}enabled: true/);
+    assert.fileContent('devhost/settings.yml', /aws_access_key_id: testKey/);
+    assert.fileContent('devhost/settings.yml', /aws_secret_access_key: testSecKey/);
+  });
+});
